00722e7ca577c9e98223fed4af1466379e1b6679
xrabcde <xrabcde@gmail.com>
2022-10-20 22:06:20 +0900

    feat: 로그인 기능 추가에 따른 예약 생성/삭제 리팩터링

--- src/main/java/nextstep/reservation/ReservationService.java
+++ src/main/java/nextstep/reservation/ReservationService.java
@@ -1,60 +1,94 @@
 package nextstep.reservation;
 
+import nextstep.auth.AuthenticationException;
+import nextstep.member.Member;
+import nextstep.member.MemberDao;
 import nextstep.schedule.Schedule;
 import nextstep.schedule.ScheduleDao;
 import nextstep.support.DuplicateEntityException;
-import nextstep.theme.Theme;
 import nextstep.theme.ThemeDao;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ReservationService {
-    public final ReservationDao reservationDao;
-    public final ThemeDao themeDao;
-    public final ScheduleDao scheduleDao;
+    private final ReservationDao reservationDao;
+    private final ThemeDao themeDao;
+    private final ScheduleDao scheduleDao;
 
-    public ReservationService(ReservationDao reservationDao, ThemeDao themeDao, ScheduleDao scheduleDao) {
+    private final MemberDao memberDao;
+
+    public ReservationService(ReservationDao reservationDao, ThemeDao themeDao, ScheduleDao scheduleDao, MemberDao memberDao) {
         this.reservationDao = reservationDao;
         this.themeDao = themeDao;
         this.scheduleDao = scheduleDao;
+        this.memberDao = memberDao;
     }
 
-    public Long create(ReservationRequest reservationRequest) {
-        Schedule schedule = scheduleDao.findById(reservationRequest.getScheduleId());
-        if (schedule == null) {
-            throw new NullPointerException();
-        }
+    public Long create(Long memberId, ReservationRequest reservationRequest) {
+        Schedule schedule = findSchedule(reservationRequest.getScheduleId());
+        Member member = findMember(memberId);
+        checkReservationAvailable(schedule.getId());
 
-        List<Reservation> reservation = reservationDao.findByScheduleId(schedule.getId());
-        if (!reservation.isEmpty()) {
-            throw new DuplicateEntityException();
-        }
-
-        Reservation newReservation = new Reservation(
-                schedule,
-                reservationRequest.getName()
-        );
-
+        Reservation newReservation = new Reservation(schedule, member);
         return reservationDao.save(newReservation);
     }
 
+    private Schedule findSchedule(Long scheduleId) {
+        try {
+            return scheduleDao.findById(scheduleId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new NullPointerException("존재하지 않는 스케줄입니다.");
+        }
+    }
+
+    private Member findMember(Long memberId) {
+        try {
+            return memberDao.findById(memberId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new NullPointerException("존재하지 않는 멤버입니다.");
+        }
+    }
+
+    private void checkReservationAvailable(Long scheduleId) {
+        if (reservationDao.existsByScheduleId(scheduleId)) {
+            throw new DuplicateEntityException("이미 예약된 스케줄입니다.");
+        }
+    }
+
     public List<Reservation> findAllByThemeIdAndDate(Long themeId, String date) {
-        Theme theme = themeDao.findById(themeId);
-        if (theme == null) {
-            throw new NullPointerException();
+        try {
+            themeDao.findById(themeId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new NullPointerException("존재하지 않는 테마입니다.");
         }
 
         return reservationDao.findAllByThemeIdAndDate(themeId, date);
     }
 
-    public void deleteById(Long id) {
-        Reservation reservation = reservationDao.findById(id);
-        if (reservation == null) {
-            throw new NullPointerException();
-        }
+    public void deleteById(Long memberId, Long id) {
+        Reservation reservation = findReservation(id);
+        Member member = findMember(memberId);
+        checkMyReservation(reservation, member);
 
-        reservationDao.deleteById(id);
+        if (reservationDao.deleteById(id) == 0) {
+            throw new NullPointerException("존재하지 않는 예약입니다.");
+        }
+    }
+
+    private void checkMyReservation(Reservation reservation, Member member) {
+        if (!reservation.isMine(member)) {
+            throw new AuthenticationException("해당 예약에 대한 권한이 없습니다");
+        }
+    }
+
+    private Reservation findReservation(Long reservationId) {
+        try {
+            return reservationDao.findById(reservationId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new NullPointerException("존재하지 않는 예약입니다.");
+        }
     }
 }
