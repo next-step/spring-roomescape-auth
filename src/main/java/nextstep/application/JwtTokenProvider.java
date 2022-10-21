58ca6dd969b9fb33c7d058490a32e9801829b427
Daeun Lee <leedaeun.dev@gmail.com>
2022-10-22 01:17:39 +0900

    style: 코드 스타일

--- src/main/java/nextstep/application/JwtTokenProvider.java
+++ src/main/java/nextstep/application/JwtTokenProvider.java
@@ -34,7 +34,11 @@ public class JwtTokenProvider {
     }
 
     public String getPrincipal(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser()
+            .setSigningKey(secretKey)
+            .parseClaimsJws(token)
+            .getBody()
+            .getSubject();
     }
 
     public List<String> getRoles(String token) {
@@ -47,7 +51,10 @@ public class JwtTokenProvider {
 
     public boolean validateToken(String token) {
         try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            Jws<Claims> claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token);
+
             return !claims.getBody().getExpiration().before(new Date());
 
         } catch (JwtException | IllegalArgumentException e) {
