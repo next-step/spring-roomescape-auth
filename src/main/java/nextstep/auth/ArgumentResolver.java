d167208117bf7e3534f6408f8d8cbcde2c660a87
xrabcde <xrabcde@gmail.com>
2022-10-23 20:19:55 +0900

    feat: 관리자 관련 기능에 관리자 기능 보호 구현

--- src/main/java/nextstep/auth/ArgumentResolver.java
+++ src/main/java/nextstep/auth/ArgumentResolver.java
@@ -1,7 +1,6 @@
 package nextstep.auth;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -23,18 +22,9 @@ public class ArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        String token = extractToken((HttpServletRequest) webRequest.getNativeRequest());
+        String token = jwtTokenProvider.extractToken((HttpServletRequest) webRequest.getNativeRequest());
         jwtTokenProvider.validateToken(token);
         String principal = jwtTokenProvider.getPrincipal(token);
         return LoginMember.from(principal);
-    }
-
-    private String extractToken(HttpServletRequest request) {
-        try {
-            String header = request.getHeader(HttpHeaders.AUTHORIZATION);
-            return header.split(" ")[1];
-        } catch (Exception e) {
-            throw new AuthenticationException("로그인 후 이용가능합니다.");
-        }
     }
 }
--- src/main/java/nextstep/auth/JwtTokenProvider.java
+++ src/main/java/nextstep/auth/JwtTokenProvider.java
@@ -2,8 +2,10 @@ package nextstep.auth;
 
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.List;
 
@@ -27,12 +29,21 @@ public class JwtTokenProvider {
                 .compact();
     }
 
+    public String extractToken(HttpServletRequest request) {
+        try {
+            String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+            return header.split(" ")[1];
+        } catch (Exception e) {
+            throw new AuthenticationException("로그인 후 이용가능합니다.");
+        }
+    }
+
     public String getPrincipal(String token) {
         return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
     }
 
-    public List<String> getRoles(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("roles", List.class);
+    public String getRole(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("roles", String.class);
     }
 
     public boolean validateToken(String token) {
