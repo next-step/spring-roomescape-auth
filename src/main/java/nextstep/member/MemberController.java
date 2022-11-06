a3e0037ae6e6e84e44ed41816dd4c1c2466eb451
livenow14 <xowjd41@naver.com>
2022-11-07 01:11:47 +0900

    [1단계 - 로그인] feat: 내 정보 조회하기 - 토큰을 이용하여 본인 정보 응답하기

--- src/main/java/nextstep/member/MemberController.java
+++ src/main/java/nextstep/member/MemberController.java
@@ -1,29 +1,45 @@
 package nextstep.member;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import nextstep.auth.AuthenticationException;
+import nextstep.auth.JwtTokenProvider;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController {
-    private MemberService memberService;
 
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
+  private final MemberService memberService;
+  private final JwtTokenProvider tokenProvider;
 
-    @PostMapping
-    public ResponseEntity createMember(@RequestBody MemberRequest memberRequest) {
-        Long id = memberService.create(memberRequest);
-        return ResponseEntity.created(URI.create("/members/" + id)).build();
-    }
 
-    @GetMapping("/me")
-    public ResponseEntity me() {
-        Long id = 1L;
-        Member member = memberService.findById(id);
-        return ResponseEntity.ok(member);
+  @PostMapping
+  public ResponseEntity createMember(@RequestBody MemberRequest memberRequest) {
+    Long id = memberService.create(memberRequest);
+    return ResponseEntity.created(URI.create("/members/" + id)).build();
+  }
+
+  @GetMapping("/me")
+  public ResponseEntity me(@RequestHeader String authorization) {
+    if (!tokenProvider.validateToken(authorization)) {
+      throw new AuthenticationException("인증되지 않은 토큰입니다.");
     }
+    var principal = tokenProvider.getPrincipal(authorization);
+    Member member = memberService.findById(Long.valueOf(principal));
+    return ResponseEntity.ok(member);
+  }
+
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<String> handle(RuntimeException exception) {
+    return ResponseEntity.badRequest().body(exception.getMessage());
+  }
 }
