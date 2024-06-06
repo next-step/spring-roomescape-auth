6617e5a87b395e84107cf6476cf2fe35f36c29d7
parkjun5 <58926619+parkjun5@users.noreply.github.com>
2024-06-06 14:37:43 +0900

    😺 Step1 1단계 채리픽 - 예약 기능 리뷰 요청 (#53)
    * feat: Define Reservation Domain And ReservationTime Domain
    
    * feat: Add JDBC repositories for Create, Read, Delete operations
    
    * feat: Add common support settings to the project
    
    - Add index controller to support pages.
    - ReservationDateChecker to validate dates.
    - Add properties datasource setting.
    
    * feat: Implement Reservation Application Layer
    
    - Add ReservationCanceler to cancel Reservation.
    - Add ReservationFinder to find reservations.
    - Add ReservationRecorder to record new reservations.
    
    * feat: Implement Reservation Presentation Layer
    
    * feat: Implement ReservationTime Application Layer
    
    - Add ReservationTimeDeleter to delete ReservationTime.
    - Add ReservationTimeFinder to find reservationsTime.
    - Add ReservationTimeSaver to save new reservationsTime.
    
    * feat: Implement ReservationTime Presentation Layer
    
    * test: Update tests for reservation changes and add new feature for reservation time
    
    * test: Add more application tests
    
    - Add tests for reservation applications.
    - Add tests for reservation time applications.
    
    ---------
    
    Co-authored-by: MZC01-PARKJUN5 <parkjun5@mz.co.kr>
    
    (cherry picked from commit 43cdfdb53299f0b668e5224217cf16143c93535b)

---
+++ src/test/java/roomescape/MissionStepTest.java
@@ -0,0 +1,99 @@
+package roomescape;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class MissionStepTest {
+
+    @Test
+    void page() {
+        RestAssured.given().log().all()
+                .when().get("/")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    void reservation() {
+        Map<String, String> timeParams = new HashMap<>();
+        timeParams.put("startAt", "10:00");
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(timeParams)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+
+        Map<String, String> reservationParams = new HashMap<>();
+        reservationParams.put("name", "브라운");
+        reservationParams.put("date", "2026-08-05");
+        reservationParams.put("timeId", "1");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationParams)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @Test
+    void reservationTime() {
+        Map<String, String> timeParams = new HashMap<>();
+        timeParams.put("startAt", "10:00");
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(timeParams)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().get("/times")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+
+        RestAssured.given().log().all()
+                .when().delete("/times/1")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/times")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+}
