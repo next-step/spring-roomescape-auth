06df9cb8a64228e0d0209ada608fb6cc900a928a
MZC01-PARKJUN5 <parkjun5@mz.co.kr>
2024-06-14 17:44:20 +0900

    refactor: data.sql 분리
     - schema.sql과 data.sql으로 분리하여 데이터 조작 언어와 데이터 정의 언어를 별도로 관리

---
+++ src/test/resources/schema_test.sql
@@ -0,0 +1,45 @@
+CREATE TABLE reservation_time
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE theme
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    thumbnail   VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE reservation
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL,
+    date     VARCHAR(255) NOT NULL,
+    time_id  BIGINT,
+    theme_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id) ON DELETE RESTRICT,
+    FOREIGN KEY (theme_id) REFERENCES theme (id) ON DELETE RESTRICT
+);
+
+CREATE TABLE member
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE member_role
+(
+    id        BIGINT       NOT NULL AUTO_INCREMENT,
+    name      VARCHAR(255) NOT NULL,
+    member_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE RESTRICT
+);
