2083bb05571d871c8742e4f0123ac993f81eb942
leeedohyun <ldh19738@gmail.com>
2024-06-15 15:43:50 +0900

    feat: 토큰 생성하는 Provider 생성

---
+++ src/main/java/roomescape/jwt/JwtTokenProvider.java
@@ -0,0 +1,33 @@
+package roomescape.jwt;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtTokenProvider {
+
+    private final SecretKey secretKey;
+    private final long expiredMs;
+
+    public JwtTokenProvider(@Value("${jwt.secret}") String secret, @Value("${jwt.expire}") long validityInMilliseconds) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(UTF_8));
+        this.expiredMs = validityInMilliseconds;
+    }
+
+    public String createJwt(String email) {
+        return Jwts.builder()
+                .claim("email", email)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
