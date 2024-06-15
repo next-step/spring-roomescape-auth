c58a5bf682872bc2289aaf47a6d9e7063fbb1481
psy3720 <qkrtmddyd12@naver.com>
2024-06-15 13:36:51 +0900

    feat: 로그아웃 추가

--- src/main/java/roomescape/controller/front/ReservationViewController.java
+++ src/main/java/roomescape/controller/front/ReservationViewController.java
@@ -3,6 +3,7 @@ package roomescape.controller.front;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
@@ -71,5 +72,20 @@ public class ReservationViewController {
             }
         }
         return null;
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    cookie.setMaxAge(0);
+                    cookie.setPath("/");
+                    response.addCookie(cookie);
+                }
+            }
+        }
+        return ResponseEntity.ok().build();
     }
 }
