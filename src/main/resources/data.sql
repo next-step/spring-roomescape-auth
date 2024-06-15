048c809c202835ec5f6c0a21c74093d6fda555f4
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2024-06-15 20:05:41 +0900

    polishing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,7 +12,7 @@ VALUES ('고양이 테마', '고양이 카페에서 탈출하는 테마입니다
        ('포켓몬 테마', '포켓몬고를 설치하셔야 합니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
 INSERT INTO member (name, email, password)
-VALUES  ('테스트_유저', 'tester@gmail.com', '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b');
+VALUES  ('테스트_유저', 'tester@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
 
 INSERT INTO member_role (name, member_id)
 VALUES  ('ADMIN', 1);
