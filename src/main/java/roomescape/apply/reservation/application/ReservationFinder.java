febd35424c7bc647542c0e9daadbe79482138332
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2024-06-15 20:05:27 +0900

    refactor: 예약을 관리자 모드로 저장/찾아오기 기능 추가

--- src/main/java/roomescape/apply/reservation/application/ReservationFinder.java
+++ src/main/java/roomescape/apply/reservation/application/ReservationFinder.java
@@ -1,7 +1,11 @@
 package roomescape.apply.reservation.application;
 
 import org.springframework.stereotype.Service;
+import roomescape.apply.auth.ui.dto.LoginMember;
+import roomescape.apply.member.application.MemberFinder;
+import roomescape.apply.member.domain.Member;
 import roomescape.apply.reservation.domain.repository.ReservationRepository;
+import roomescape.apply.reservation.ui.dto.ReservationAdminResponse;
 import roomescape.apply.reservation.ui.dto.ReservationResponse;
 
 import java.util.List;
@@ -11,15 +15,25 @@ import java.util.Optional;
 public class ReservationFinder {
 
     private final ReservationRepository reservationRepository;
+    private final MemberFinder memberFinder;
 
-    public ReservationFinder(ReservationRepository reservationRepository) {
+    public ReservationFinder(ReservationRepository reservationRepository, MemberFinder memberFinder) {
         this.reservationRepository = reservationRepository;
+        this.memberFinder = memberFinder;
     }
 
     public List<ReservationResponse> findAll() {
         return reservationRepository.findAll()
                 .stream()
                 .map(it -> ReservationResponse.from(it, it.getTheme(), it.getTime()))
+                .toList();
+    }
+
+    public List<ReservationAdminResponse> findAllForAdmin(LoginMember loginMember) {
+        Member member = memberFinder.findOneNameById(loginMember.id());
+        return reservationRepository.findAll()
+                .stream()
+                .map(it -> ReservationAdminResponse.from(it, it.getTheme(), it.getTime(), member))
                 .toList();
     }
 
