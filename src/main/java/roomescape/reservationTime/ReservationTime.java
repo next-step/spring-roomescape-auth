ecabe99ba976726c9d282506398482a6192161fa
hhhhhwi <aab555586@gmail.com>
2024-06-16 16:23:03 +0900

    feat: 예약 기능 프로젝트 코드 세팅

---
+++ src/main/java/roomescape/reservationTime/ReservationTime.java
@@ -0,0 +1,27 @@
+package roomescape.reservationTime;
+
+import java.time.LocalTime;
+
+public class ReservationTime {
+
+    private Long id;
+
+    private LocalTime startAt;
+
+    public ReservationTime(Long id, LocalTime startAt) {
+        this.id = id;
+        this.startAt = startAt;
+    }
+
+    public ReservationTime(String startAtStr) {
+        this(null, LocalTime.parse(startAtStr));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalTime getStartAt() {
+        return startAt;
+    }
+}
