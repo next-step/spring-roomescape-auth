1b756ce1ebe3ac2b62c9950b86b16f7a02c2358f
eottabom <snoopy12oyk@gmail.com>
2024-06-17 16:21:06 +0900

    STEP1 : 로그인 구현 - EnableConfigurationProperties 설정 - 스키마 추가 - 불필요한 테스트 클래스 삭제

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -1,11 +1,14 @@
 package roomescape;
 
+import roomescape.config.JwtProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({ JwtProperties.class })
 @SpringBootApplication
 public class RoomescapeApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoomescapeApplication.class, args);
