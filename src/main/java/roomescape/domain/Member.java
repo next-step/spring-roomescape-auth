104278269ac836e8b06566c8e16e815942ab76d6
eottabom <snoopy12oyk@gmail.com>
2024-06-17 16:20:26 +0900

    STEP1 : 로그인 구현 - 멤버 기능 구현

---
+++ src/main/java/roomescape/domain/Member.java
@@ -0,0 +1,112 @@
+package roomescape.domain;
+
+import java.util.Objects;
+
+public class Member {
+
+	private Long id;
+
+	private String name;
+
+	private String email;
+
+	private String password;
+
+	private String role;
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public String getRole() {
+		return this.role;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Member that = (Member) o;
+		return Objects.equals(this.id, that.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.id);
+	}
+
+	@Override
+	public String toString() {
+		//@formatter: off
+		return "Member{" +
+				"id=" + this.id +
+				", name='" + this.name + '\'' +
+				", email='" + this.email + '\'' +
+				", password='" + this.password + '\'' +
+				", role='" + this.role + '\'' +
+				'}';
+		//@formatter: on
+	}
+
+	public static final class Builder {
+
+		private final Member member;
+
+		public Builder() {
+			this.member = new Member();
+		}
+
+		public Builder id(long id) {
+			this.member.id = id;
+			return this;
+		}
+
+		public Builder name(String name) {
+			this.member.name = name;
+			return this;
+		}
+
+		public Builder email(String email) {
+			this.member.email = email;
+			return this;
+		}
+
+		public Builder password(String password) {
+			this.member.password = password;
+			return this;
+		}
+
+		public Builder role(String role) {
+			this.member.role = role;
+			return this;
+		}
+
+		public Member build() {
+			return this.member;
+		}
+	}
+}
---
+++ src/test/java/roomescape/service/MemberServiceTests.java
@@ -0,0 +1,131 @@
+package roomescape.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import roomescape.controller.dto.LoginRequest;
+import roomescape.controller.dto.MemberRequest;
+import roomescape.controller.dto.MemberResponse;
+import roomescape.domain.Member;
+import roomescape.domain.MemberRole;
+import roomescape.exception.ErrorCode;
+import roomescape.exception.RoomEscapeException;
+import roomescape.repository.MemberRepository;
+import roomescape.support.PasswordEncoder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+class MemberServiceTests {
+
+	@InjectMocks
+	private MemberService memberService;
+
+	@Mock
+	private MemberRepository memberRepository;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void create() {
+		// given
+		MemberRequest memberRequest = new MemberRequest("이름", "tester@gmail.com", "1234");
+		Member member = Member.builder()
+			.id(1L)
+			.name("이름")
+			.email("tester@gmail.com")
+			.password("encodedPassword")
+			.role(MemberRole.USER.name())
+			.build();
+
+		MemberResponse memberResponse = new MemberResponse(1L, "이름", "tester@gmail.com", MemberRole.USER.name());
+
+		given(this.memberRepository.isExists(memberRequest.name())).willReturn(false);
+		given(this.memberRepository.save(any(Member.class))).willReturn(member);
+
+		// when
+		var createdMember = this.memberService.create(memberRequest);
+
+		// then
+		assertThat(createdMember).isEqualTo(memberResponse);
+		assertThat(createdMember.id()).isEqualTo(1L);
+		assertThat(createdMember.name()).isEqualTo("이름");
+		assertThat(createdMember.email()).isEqualTo("tester@gmail.com");
+		assertThat(createdMember.role()).isEqualTo(MemberRole.USER.name());
+	}
+
+	@Test
+	void createWhenMemberAlreadyExists() {
+		// given
+		MemberRequest memberRequest = new MemberRequest("이름", "tester@gmail.com", "1234");
+
+		given(this.memberRepository.isExists(memberRequest.name())).willReturn(true);
+
+		// when, then
+		assertThatThrownBy(() -> this.memberService.create(memberRequest)).isInstanceOf(RoomEscapeException.class)
+			.hasMessageContaining(ErrorCode.DUPLICATE_MEMBER.getMessage());
+	}
+
+	@Test
+	void findMemberByLoginRequest() {
+		// given
+		LoginRequest loginRequest = new LoginRequest("tester@gmail.com", PasswordEncoder.encode("1234"));
+		Member member = Member.builder()
+				.id(1L)
+				.name("이름")
+				.email("tester@gmail.com")
+				.password(PasswordEncoder.encode("1234"))
+				.role(MemberRole.USER.name())
+				.build();
+		MemberResponse memberResponse = new MemberResponse(1L, "이름", "tester@gmail.com", MemberRole.USER.name());
+
+		given(this.memberRepository.findByEmail(loginRequest.email())).willReturn(member);
+
+		// when
+		MemberResponse actualResponse = this.memberService.findMemberByLoginRequest(loginRequest);
+
+		// then
+		assertThat(actualResponse).isEqualTo(memberResponse);
+	}
+
+	@Test
+	void findMemberByLoginRequestWhenMemberDoesNotExist() {
+		// given
+		LoginRequest loginRequest = new LoginRequest("tester@gmail.com", PasswordEncoder.encode("1234"));
+
+		given(this.memberRepository.findByEmail(loginRequest.email())).willReturn(null);
+
+		// when, then
+		assertThatThrownBy(() -> this.memberService.findMemberByLoginRequest(loginRequest))
+				.isInstanceOf(RoomEscapeException.class)
+				.hasMessageContaining(ErrorCode.NOT_FOUND_MEMBER.getMessage());
+	}
+
+	@Test
+	void findMemberByLoginRequestWhenPasswordDoesNotMatch() {
+		// given
+		LoginRequest loginRequest = new LoginRequest("tester@gmail.com", PasswordEncoder.encode("4444"));
+		Member member = Member.builder()
+				.id(1L)
+				.name("이름")
+				.email("tester@gmail.com")
+				.password(PasswordEncoder.encode("1234"))
+				.role(MemberRole.USER.name())
+				.build();
+
+		given(this.memberRepository.findByEmail(loginRequest.email())).willReturn(member);
+
+		// when, then
+		assertThatThrownBy(() -> this.memberService.findMemberByLoginRequest(loginRequest))
+				.isInstanceOf(RoomEscapeException.class)
+				.hasMessageContaining(ErrorCode.INVALID_PASSWORD.getMessage());
+	}
+
+}
