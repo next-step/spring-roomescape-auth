cda09ba7a159d457b36a2f76dd6bd9efef5560b6
MZC01-PARKJUN5 <parkjun5@mz.co.kr>
2024-06-17 17:50:43 +0900

    refactor: remove duplicate TokenFinder WebRequestTokenFinder
    - NativeWebRequest에서 쿠키를 가져오지 말고 HttpServletRequest로 변환하여 기존 TokenFinder를 사용하도록 변경
    - 사용하지 않게된 WebRequestTokenFinder 제거

--- src/main/java/roomescape/support/handler/LoginMemberArgumentResolver.java
+++ src/main/java/roomescape/support/handler/LoginMemberArgumentResolver.java
@@ -1,15 +1,15 @@
 package roomescape.support.handler;
 
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import roomescape.apply.auth.application.JwtTokenManager;
-import roomescape.apply.auth.application.exception.TokenNotFoundException;
 import roomescape.apply.auth.ui.dto.LoginMember;
-import roomescape.support.WebRequestTokenFinder;
+import roomescape.support.ServletRequestTokenFinder;
 
 public record LoginMemberArgumentResolver(
         JwtTokenManager jwtTokenManager
@@ -21,13 +21,17 @@ public record LoginMemberArgumentResolver(
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter,
-                                  ModelAndViewContainer mavContainer,
-                                  NativeWebRequest webRequest,
-                                  WebDataBinderFactory binderFactory
+    public LoginMember resolveArgument(@Nonnull MethodParameter parameter,
+                                       ModelAndViewContainer mavContainer,
+                                       @Nonnull NativeWebRequest webRequest,
+                                       WebDataBinderFactory binderFactory
     ) {
-        String token = WebRequestTokenFinder.getTokenByRequestCookies(webRequest);
-        jwtTokenManager.validateToken(token);
-        return jwtTokenManager.getMemberEmailAndNameBy(token);
+        if (webRequest instanceof HttpServletRequest httpServletRequest) {
+            String token = ServletRequestTokenFinder.getTokenByRequestCookies(httpServletRequest);
+            jwtTokenManager.validateToken(token);
+            return jwtTokenManager.getMemberEmailAndNameBy(token);
+        }
+
+        throw new IllegalArgumentException("HttpServletRequest 객체를 가져올 수 없습니다.");
     }
 }
