55018d2cfb4ab10cd0de6aa4651724dc87ebf566
gomshiki <gomshiki@gmail.com>
2024-06-18 13:14:16 +0900

    rebase(project) : 프로젝트 리베이스
    PR된 원격저장소 기준 rebase 수행

---
+++ src/main/java/roomescape/reservation/ReservationService.java
@@ -0,0 +1,84 @@
+package roomescape.reservation;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import roomescape.reservationTime.ReservationTime;
+import roomescape.reservationTime.ReservationTimePolicy;
+import roomescape.reservationTime.ReservationTimeResponseDto;
+
+import java.util.List;
+
+@Service
+public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
+    public List<ReservationResponseDto> findAll() {
+        List<Reservation> reservations = reservationRepository.findAll();
+        return reservations.stream()
+                .map(
+                        reservation -> new ReservationResponseDto.Builder()
+                                .id(reservation.getId())
+                                .name(reservation.getName())
+                                .date(reservation.getDate())
+                                .reservationTimeResponseDto(new ReservationTimeResponseDto(
+                                        reservation.getReservationTime().getId(),
+                                        reservation.getReservationTime().getStartAt()))
+                                .build()
+                ).toList();
+    }
+
+    @Transactional
+    public ReservationResponseDto save(ReservationRequestDto reservationRequestDto) {
+        final Reservation reservation = new Reservation.Builder().name(reservationRequestDto.getName())
+                .date(reservationRequestDto.getDate())
+                .reservationTime(
+                        new ReservationTime(
+                                reservationRequestDto.getReservationTimeRequestDto().getId(),
+                                reservationRequestDto.getReservationTimeRequestDto().getStartAt()
+                                ))
+                .build();
+
+        final Long savedId = reservationRepository.save(reservation);
+        final Reservation savedReservation = reservationRepository.findById(savedId);
+
+        return new ReservationResponseDto.Builder()
+                .id(savedReservation.getId())
+                .name(savedReservation.getName())
+                .date(savedReservation.getDate())
+                .reservationTimeResponseDto(
+                        new ReservationTimeResponseDto(savedReservation.getReservationTime().getId(),
+                                savedReservation.getReservationTime().getStartAt()))
+                .build();
+    }
+
+
+    @Transactional
+    public void delete(Long id) {
+        final boolean isExistedReservation = reservationRepository.existsById(id);
+        if (!isExistedReservation) {
+            throw new IllegalArgumentException("해당 예약이 존재하지 않습니다.");
+        }
+        reservationRepository.deleteById(id);
+    }
+
+
+    public ReservationResponseDto findById(Long id) {
+        Reservation reservation = reservationRepository.findById(id);
+        return new ReservationResponseDto.Builder()
+                .id(reservation.getId())
+                .name(reservation.getName())
+                .date(reservation.getDate())
+                .reservationTimeResponseDto(
+                        new ReservationTimeResponseDto(
+                                reservation.getReservationTime().getId(),
+                                reservation.getReservationTime().getStartAt()))
+                .build();
+    }
+
+
+}
