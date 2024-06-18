a212f79b4f3c37ec9d9cb42279134f3f8bb9ef04
leeedohyun <ldh19738@gmail.com>
2024-06-18 18:43:51 +0900

    refactor: 반복되는 테스트 데이터 제거

--- src/test/java/roomescape/theme/infrastructure/JdbcThemeRepositoryTest.java
+++ src/test/java/roomescape/theme/infrastructure/JdbcThemeRepositoryTest.java
@@ -26,22 +26,23 @@ class JdbcThemeRepositoryTest {
 
     private ThemeRepository themeRepository;
 
+    private Theme theme;
+    private Theme savedTheme;
+
     @BeforeEach
     void setUp() {
         themeRepository = new JdbcThemeRepository(jdbcTemplate, dataSource);
+
+        theme = new Theme("레벨2 탈출","우테코 레벨2를 탈출하는 내용입니다.",
+                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg");
+        savedTheme = themeRepository.save(theme);
     }
 
     @Test
     @DisplayName("테마를 저장한다.")
     void save() {
-        // given
-        Theme theme = new Theme("레벨2 탈출","우테코 레벨2를 탈출하는 내용입니다.",
-                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg");
-
-        // when
         Theme savedTheme = themeRepository.save(theme);
 
-        // then
         assertThat(savedTheme.getId()).isNotNull();
         assertThat(savedTheme).usingRecursiveComparison()
                 .ignoringFields("id")
@@ -51,15 +52,8 @@ class JdbcThemeRepositoryTest {
     @Test
     @DisplayName("테마를 조회한다.")
     void findById() {
-        // given
-        Theme theme = new Theme("레벨2 탈출","우테코 레벨2를 탈출하는 내용입니다.",
-                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg");
-        Theme savedTheme = themeRepository.save(theme);
-
-        // when
         Theme findTheme = themeRepository.findById(savedTheme.getId()).get();
 
-        // then
         assertThat(findTheme).usingRecursiveComparison()
                 .ignoringFields("id")
                 .isEqualTo(theme);
@@ -68,30 +62,16 @@ class JdbcThemeRepositoryTest {
     @Test
     @DisplayName("모든 테마를 조회한다.")
     void findAll() {
-        // given
-        Theme theme = new Theme("레벨2 탈출","우테코 레벨2를 탈출하는 내용입니다.",
-                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg");
-        themeRepository.save(theme);
-
-        // when
         List<Theme> themes = themeRepository.findAll();
 
-        // then
         assertThat(themes).hasSize(1);
     }
 
     @Test
     @DisplayName("테마가 존재하면 TRUE를 반환한다.")
     void existsById_ReturnTrue() {
-        // given
-        Theme theme = new Theme("레벨2 탈출","우테코 레벨2를 탈출하는 내용입니다.",
-                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg");
-        Theme savedTheme = themeRepository.save(theme);
-
-        // when
         boolean result = themeRepository.existsById(savedTheme.getId());
 
-        // then
         assertThat(result).isTrue();
     }
 
