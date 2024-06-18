edc335e8af58a618ce55b24b99fac49400895759
leeedohyun <ldh19738@gmail.com>
2024-06-18 20:12:30 +0900

    test: 테스트 데이터 중복 삭제

--- src/test/java/roomescape/time/presntation/ReservationTimeControllerTest.java
+++ src/test/java/roomescape/time/presntation/ReservationTimeControllerTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.is;
 import java.time.LocalDate;
 import java.util.Map;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,10 +20,21 @@ import roomescape.reservation.dto.ReservationCreateRequest;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationTimeControllerTest {
 
+    @BeforeEach
+    void setUp() {
+        Map<String, String> params = Map.of("startAt", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all();
+    }
+
     @Test
     @DisplayName("시간 추가를 한다.")
     void testCreateReservationTime() {
-        Map<String, String> params = Map.of("startAt", "10:00");
+        Map<String, String> params = Map.of("startAt", "12:00");
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -55,27 +67,12 @@ class ReservationTimeControllerTest {
                 .body(params)
                 .when().post("/times")
                 .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/times")
-                .then().log().all()
                 .statusCode(HttpStatus.BAD_REQUEST.value());
     }
     
     @Test
     @DisplayName("모든 시간을 조회한다.")
     void testGetReservationTimes() {
-        Map<String, String> params = Map.of("startAt", "10:00");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/times")
-                .then().log().all();
-
         RestAssured.given().log().all()
                 .when().get("/times")
                 .then().log().all()
@@ -86,14 +83,6 @@ class ReservationTimeControllerTest {
     @Test
     @DisplayName("시간을 삭제한다.")
     void testDeleteReservationTime() {
-        Map<String, String> params = Map.of("startAt", "10:00");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/times")
-                .then().log().all();
-
         RestAssured.given().log().all()
                 .when().delete("/times/1")
                 .then().log().all()
@@ -103,16 +92,6 @@ class ReservationTimeControllerTest {
     @Test
     @DisplayName("시간 삭제 시 예약된 시간이면 실패한다.")
     void testDeleteReservationTime_Fail() {
-        // ReservationTime 생성
-        Map<String, String> param = Map.of("startAt", "10:00");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(param)
-                .when().post("/times")
-                .then().log().all()
-                .extract();
-
         // Theme 생성
         Map<String, String> params = Map.of(
                 "name", "레벨2 탈출",
@@ -148,19 +127,11 @@ class ReservationTimeControllerTest {
     @DisplayName("예약 가능한 시간을 조회한다.")
     void getAvailableTimes() {
         // 시간 생성
-        Map<String, String> param1 = Map.of("startAt", "10:00");
+        Map<String, String> param = Map.of("startAt", "12:00");
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
-                .body(param1)
-                .when().post("/times")
-                .then().log().all();
-
-        Map<String, String> param2 = Map.of("startAt", "12:00");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(param2)
+                .body(param)
                 .when().post("/times")
                 .then().log().all();
 
