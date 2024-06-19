a6bacce4448e07c475fc6b9801ff10472b901e1a
hhhhhwi <aab555586@gmail.com>
2024-06-19 17:45:11 +0900

    feat: 회원에 대한 객체 생성 및 DDL 작성

---
+++ src/main/java/roomescape/member/Member.java
@@ -0,0 +1,29 @@
+package roomescape.member;
+
+import org.thymeleaf.util.StringUtils;
+
+public class Member {
+
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    public Member(Long id, String email, String password, String name) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    public boolean isMatchedPassword(String password) {
+        return StringUtils.equals(this.password, password);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE reservation_time
 CREATE TABLE theme
 (
     id          BIGINT       NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
+    name        VARCHAR(30) NOT NULL,
     description VARCHAR(255) NOT NULL,
     thumbnail VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
@@ -25,3 +25,14 @@ CREATE TABLE reservation
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
+
+CREATE TABLE member
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    email VARCHAR(45) NOT NULL,
+    password VARCHAR(45) NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO member (email, password, name) VALUES ('test@test.com', 'password', 'test');
