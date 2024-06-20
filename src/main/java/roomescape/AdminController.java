14831e2f8d35926e33ca7b189fad97575beab954
leeedohyun <ldh19738@gmail.com>
2024-06-21 00:03:39 +0900

    feat: 어드민 예약 API 구현

--- src/main/java/roomescape/AdminController.java
+++ src/main/java/roomescape/AdminController.java
@@ -1,10 +1,28 @@
 package roomescape;
 
+import java.net.URI;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import roomescape.reservation.application.ReservationService;
+import roomescape.reservation.dto.ReservationCreateRequest;
+import roomescape.reservation.dto.ReservationResponse;
 
 @Controller
 public class AdminController {
+
+    private final ReservationService reservationService;
+
+    public AdminController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
 
     @GetMapping("/")
     public String index() {
@@ -14,6 +32,13 @@ public class AdminController {
     @GetMapping("/admin/reservation")
     public String reservation() {
         return "admin/reservation";
+    }
+
+    @PostMapping("/admin/reservation")
+    @ResponseBody
+    public ResponseEntity<ReservationResponse> reservationPost(@RequestBody @Valid ReservationCreateRequest request) {
+        ReservationResponse response = reservationService.save(request, request.memberId());
+        return ResponseEntity.created(URI.create("/reservations/" + response.id())).build();
     }
 
     @GetMapping("/admin/time")
--- src/main/java/roomescape/reservation/dto/ReservationCreateRequest.java
+++ src/main/java/roomescape/reservation/dto/ReservationCreateRequest.java
@@ -2,16 +2,20 @@ package roomescape.reservation.dto;
 
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import roomescape.reservation.domain.Reservation;
 import roomescape.theme.domain.Theme;
 import roomescape.time.domain.ReservationTime;
 
-public record ReservationCreateRequest(@NotBlank(message = "예약 날짜는 필수 입력 값입니다.")
+public record ReservationCreateRequest(@NotNull(message = "날짜는 필수 입력 값입니다.")
                                        String date,
+                                       @NotNull(message = "테마 ID는 필수 입력 값입니다.")
+                                       Long themeId,
+                                       @NotNull(message = "시간 ID는 필수 입력 값입니다.")
                                        Long timeId,
-                                       Long themeId) {
+                                       @NotNull(message = "회원 ID는 필수 입력 값입니다.")
+                                       Long memberId) {
 
     public Reservation toReservation(String name, ReservationTime savedReservationTime, Theme savedTheme) {
         return new Reservation(name, LocalDate.parse(date), savedReservationTime, savedTheme);
