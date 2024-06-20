6f9cb8bdca18a894859dce5af0e7a678756140ef
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2024-06-20 21:59:35 +0900

    etc: add newbie for testing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,7 +12,13 @@ VALUES ('고양이 테마', '고양이 카페에서 탈출하는 테마입니다
        ('포켓몬 테마', '포켓몬고를 설치하셔야 합니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
 INSERT INTO member (name, email, password)
-VALUES  ('테스트_유저', 'tester@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
+VALUES  ('관리자', 'tester@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
 
 INSERT INTO member_role (name, member_id)
 VALUES  ('ADMIN', 1);
+
+INSERT INTO member (name, email, password)
+VALUES  ('신규_유저', 'newbie@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
+
+INSERT INTO member_role (name, member_id)
+VALUES  ('GUEST', 2);
