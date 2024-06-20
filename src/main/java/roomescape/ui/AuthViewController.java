1fb98627a78320736c9dba7439530e20a9dbb0da
yeeuniii <anna862700@gmail.com>
2024-06-21 01:34:21 +0900

    feat(step1): 로그인 페이지 설정

--- src/main/java/roomescape/ui/AuthViewController.java
+++ src/main/java/roomescape/ui/AuthViewController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthViewController {
+    @GetMapping
+    public String login() {
+        return "login";
+    }
+
     @GetMapping("signup")
     public String signUp() {
         return "signup";
