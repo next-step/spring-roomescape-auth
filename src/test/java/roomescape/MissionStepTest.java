00f713ede2b87bc34df024365a25eaa849803c80
leeedohyun <ldh19738@gmail.com>
2024-06-20 23:30:27 +0900

    test: admin 토큰 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,14 +1,31 @@
 package roomescape;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import roomescape.auth.dto.LoginRequest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
+
+    private String accessToken;
+
+    @BeforeEach
+    void setUp() {
+        LoginRequest loginRequest = new LoginRequest("admin@email.com", "password");
+
+        accessToken = RestAssured.given().log().all()
+                .body(loginRequest)
+                .contentType(ContentType.JSON)
+                .when().post("/login")
+                .then().log().all()
+                .extract().cookie("token");
+    }
 
     @Test
     void page() {
@@ -21,6 +38,7 @@ public class MissionStepTest {
     @Test
     void AdminReservationPage() {
         RestAssured.given().log().all()
+                .cookie("token", accessToken)
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
@@ -29,6 +47,7 @@ public class MissionStepTest {
     @Test
     void timePage() {
         RestAssured.given().log().all()
+                .cookie("token", accessToken)
                 .when().get("/admin/time")
                 .then().log().all()
                 .statusCode(200);
@@ -37,6 +56,7 @@ public class MissionStepTest {
     @Test
     void themePage() {
         RestAssured.given().log().all()
+                .cookie("token", accessToken)
                 .when().get("/admin/theme")
                 .then().log().all()
                 .statusCode(200);
