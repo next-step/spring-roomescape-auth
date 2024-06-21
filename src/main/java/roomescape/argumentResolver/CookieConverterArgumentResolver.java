8ed15da99f0eeee1b39c1b82020ba28f792ce445
JohnPrk <uo3641493@gmail.com>
2024-06-21 17:00:35 +0900

    feat : Cookie에 담긴 인증 정보를 바탕으로 멤버 객체를 만드는 로직을 HandlerMethodArgumentResolver에게 위임

---
+++ src/main/java/roomescape/argumentResolver/CookieConverterArgumentResolver.java
@@ -0,0 +1,54 @@
+package roomescape.argumentResolver;
+
+import com.sun.jdi.InternalException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import roomescape.argumentResolver.annotation.CookieConverter;
+import roomescape.domain.member.error.exception.MemberErrorCode;
+import roomescape.domain.member.error.exception.MemberException;
+import roomescape.domain.member.service.MemberService;
+
+@Component
+public class CookieConverterArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private static final String TOKEN = "token";
+
+    private final MemberService memberService;
+
+    public CookieConverterArgumentResolver(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CookieConverter.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest httpServletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (httpServletRequest != null) {
+            Cookie[] cookies = httpServletRequest.getCookies();
+            if (cookies != null) {
+                return memberService.findByToken(extractTokenFromCookie(cookies));
+            }
+            throw new MemberException(MemberErrorCode.INVALID_MEMBER_DETAILS_ERROR);
+        }
+        throw new InternalException();
+    }
+
+    private String extractTokenFromCookie(Cookie[] cookies) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(TOKEN)) {
+                return cookie.getValue();
+            }
+        }
+        throw new MemberException(MemberErrorCode.NOT_FOUND_COOKIE_ERROR);
+    }
+}
