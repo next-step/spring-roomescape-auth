43e391b8c1aaac30805c5e85decde5afab73447f
leeedohyun <ldh19738@gmail.com>
2024-06-21 09:54:46 +0900

    test: 어드민 예약 테스트

---
+++ src/test/java/roomescape/AdminControllerTest.java
@@ -0,0 +1,48 @@
+package roomescape;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import roomescape.auth.dto.LoginRequest;
+import roomescape.reservation.dto.ReservationCreateRequest;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class AdminControllerTest {
+
+    private String accessToken;
+
+    @BeforeEach
+    void setUp() {
+        LoginRequest loginRequest = new LoginRequest("admin@email.com", "password");
+
+        accessToken = RestAssured.given().log().all()
+                .body(loginRequest)
+                .contentType(ContentType.JSON)
+                .when().post("/login")
+                .then().log().all()
+                .extract().cookie("token");
+    }
+
+    @Test
+    void 어드민_계정으로_예약을_한다() {
+        String date = LocalDate.now().plusDays(1).toString();
+        ReservationCreateRequest request = new ReservationCreateRequest(date, 1L, 1L, 1L);
+
+        RestAssured.given().log().all()
+                .cookie("token", accessToken)
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when().post("/admin/reservation")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+}
