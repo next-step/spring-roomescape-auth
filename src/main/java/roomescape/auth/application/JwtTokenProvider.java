2bacb4cceaf04db2662f13cc6097e400dea15fc7
yeeuniii <anna862700@gmail.com>
2024-06-21 17:09:53 +0900

    feat(step1): jwt 토큰 생성 및 payload 추출 구현

---
+++ src/main/java/roomescape/auth/application/JwtTokenProvider.java
@@ -0,0 +1,37 @@
+package roomescape.auth.application;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+    @Value("${security.jwt.token.expire-length.hour}")
+    private long validTimeInHour;
+
+    public String createToken(Long memberId) {
+        Date now = new Date();
+        Date validityTime = new Date(now.getTime() + validTimeInHour * 60 * 60 * 1000);
+
+        return Jwts.builder()
+                .setSubject(memberId.toString())
+                .setIssuedAt(now)
+                .setExpiration(validityTime)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public Long extractMemberId(String token) {
+        String memberId = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJwt(token)
+                .getBody()
+                .getSubject();
+        return Long.valueOf(memberId);
+    }
+}
