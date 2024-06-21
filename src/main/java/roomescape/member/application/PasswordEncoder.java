89d6199186f53da05ff13b73bce6c909262d3dd9
yeeuniii <anna862700@gmail.com>
2024-06-21 17:32:15 +0900

    fix(step1): 비밀번호 인코딩 잘못한 것 수정

--- src/main/java/roomescape/member/application/PasswordEncoder.java
+++ src/main/java/roomescape/member/application/PasswordEncoder.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class PasswordEncoder {
     public String encode(String password) {
-        byte[] decodedBytes = Base64.decodeBase64(password);
-        return new String(decodedBytes);
+        return Base64.encodeBase64String(password.getBytes());
     }
 }
