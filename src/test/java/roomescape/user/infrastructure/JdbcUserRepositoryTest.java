67fb1c0d613afe1fcff336dacfde1e0d52dd0e74
leeedohyun <ldh19738@gmail.com>
2024-06-21 10:16:12 +0900

    test: 모든 회원 조회 API 테스트

--- src/test/java/roomescape/user/infrastructure/JdbcUserRepositoryTest.java
+++ src/test/java/roomescape/user/infrastructure/JdbcUserRepositoryTest.java
@@ -2,6 +2,8 @@ package roomescape.user.infrastructure;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -49,5 +51,12 @@ class JdbcUserRepositoryTest {
 
         // then
         assertThat(user.getEmail()).isEqualTo(email);
+    }
+
+    @Test
+    void 가입된_모든_회원을_조회한다() {
+        List<User> users = userRepository.findAll();
+
+        assertThat(users).hasSize(2);
     }
 }
