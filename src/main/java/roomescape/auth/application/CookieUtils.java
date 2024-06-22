e671981b9eb5c7e81fe80fb2e94417b669d6eafb
yeeuniii <anna862700@gmail.com>
2024-06-22 21:37:12 +0900

    feat(step1): 쿠키 배열에서 쿠키 하나만 가져오는 메서드 구현

--- src/main/java/roomescape/auth/application/CookieUtils.java
+++ src/main/java/roomescape/auth/application/CookieUtils.java
@@ -3,6 +3,8 @@ package roomescape.auth.application;
 import jakarta.servlet.http.Cookie;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class CookieUtils {
     public Cookie createCookie(String name, String value) {
@@ -10,5 +12,14 @@ public class CookieUtils {
         cookie.setHttpOnly(true);
         cookie.setPath("/");
         return cookie;
+    }
+
+    public Optional<Cookie> getCookieByName(Cookie[] cookies, String name) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(name)) {
+                return Optional.of(cookie);
+            }
+        }
+        return Optional.empty();
     }
 }
