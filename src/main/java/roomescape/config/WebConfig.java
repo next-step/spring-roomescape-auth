cc60d7f4ee4fbfb5339fad9a36d8145e640d8c89
JohnPrk <uo3641493@gmail.com>
2024-06-23 00:11:32 +0900

    feat : 사용자 권한 확인 인터셉터 추가

--- src/main/java/roomescape/config/WebConfig.java
+++ src/main/java/roomescape/config/WebConfig.java
@@ -2,9 +2,11 @@ package roomescape.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.argumentResolver.LoginArgumentResolver;
 import roomescape.domain.member.service.MemberService;
+import roomescape.interceptor.RoleCheckInterceptor;
 
 import java.util.List;
 
@@ -20,5 +22,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginArgumentResolver(memberService));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RoleCheckInterceptor(memberService)).addPathPatterns("/admin/**");
     }
 }
---
+++ src/main/java/roomescape/interceptor/RoleCheckInterceptor.java
@@ -0,0 +1,43 @@
+package roomescape.interceptor;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import roomescape.domain.member.domain.Member;
+import roomescape.domain.member.error.exception.MemberErrorCode;
+import roomescape.domain.member.error.exception.MemberException;
+import roomescape.domain.member.service.MemberService;
+
+import java.util.Arrays;
+
+public class RoleCheckInterceptor implements HandlerInterceptor {
+
+    private static final String TOKEN = "token";
+
+    private final MemberService memberService;
+
+    public RoleCheckInterceptor(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Cookie[] cookies = request.getCookies();
+        String token = extractTokenFromCookie(cookies);
+        Member member = memberService.findByToken(token);
+        if (member == null || !member.getRole().equals("ADMIN")) {
+            response.setStatus(401);
+            return false;
+        }
+        return true;
+    }
+
+    private String extractTokenFromCookie(Cookie[] cookies) {
+        return Arrays.stream(cookies)
+                .filter(cookie -> TOKEN.equals(cookie.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_COOKIE_ERROR));
+    }
+}
