a8db26e0c0cd9589ed70ea9136e70e2ac4bf5c9c
yeeuniii <anna862700@gmail.com>
2024-06-22 18:42:35 +0900

    fix(step1): login 페이지 설정 오류 해결

--- src/main/java/roomescape/ui/AuthViewController.java
+++ src/main/java/roomescape/ui/AuthViewController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthViewController {
-    @GetMapping
+    @GetMapping("login")
     public String login() {
         return "login";
     }
