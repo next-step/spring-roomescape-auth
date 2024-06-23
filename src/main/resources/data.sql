2a0859b52e1d3c653bc4ca6987d94a2bf8b9fb78
leeedohyun <ldh19738@gmail.com>
2024-06-23 21:47:23 +0900

    feat: 예약 초기 데이터 세팅

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,6 @@ INSERT INTO reservation_time (start_at) VALUES ('12:00');
 INSERT INTO theme (name, description, thumbnail) VALUES ('Theme 1', 'Description 1', 'thumbnail1.jpg');
 INSERT INTO theme (name, description, thumbnail) VALUES ('Theme 2', 'Description 1', 'thumbnail1.jpg');
 INSERT INTO theme (name, description, thumbnail) VALUES ('Theme 3', 'Description 1', 'thumbnail1.jpg');
+
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('어드민', DATEADD('DAY', 1, CURRENT_DATE), 1, 1);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('어드민', DATEADD('DAY', 1, CURRENT_DATE), 1, 2);
