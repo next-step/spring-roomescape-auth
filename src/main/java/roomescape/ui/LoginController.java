164db0dfa55f011a21f707529ad10753c2312565
dharana <dharana7723@gmail.com>
2024-06-25 04:12:42 +0900

    feat: 로그인 키인증 방법 수정 및 설정 값에 jwt.secret 추가, 로그인 인증정보 조회 api 추가

--- src/main/java/roomescape/ui/LoginController.java
+++ src/main/java/roomescape/ui/LoginController.java
@@ -1,9 +1,9 @@
 package roomescape.ui;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -13,15 +13,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import roomescape.ui.data.LoginCheckResponse;
 import roomescape.ui.data.LoginRequest;
+import roomescape.util.TokenUtil;
 
-import java.security.Key;
 import java.util.Date;
 
 @PropertySource("classpath:application.yml")
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+  @Value("${jwt.secret}")
+  private String secretKey;
 
   @GetMapping
   public String login() {
@@ -31,10 +35,10 @@ public class LoginController {
   @PostMapping
   public ResponseEntity<Cookie> login(
     @RequestBody LoginRequest loginRequest, HttpServletResponse response) {
-    Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
     String accessToken = Jwts.builder()
       .setSubject(loginRequest.getEmail())
-      .signWith(key)
+      .claim("email", loginRequest.getEmail())
+      .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
       .setIssuedAt(new Date())
       .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
       .compact();
@@ -42,5 +46,19 @@ public class LoginController {
     response.addCookie(cookie);
 
     return ResponseEntity.ok().body(cookie);
+  }
+
+  @GetMapping("/check")
+  public ResponseEntity<LoginCheckResponse> check(HttpServletRequest request) {
+    Cookie[] cookies = request.getCookies();
+    String token = TokenUtil.extractTokenFromCookie(cookies);
+    String email = Jwts.parserBuilder()
+      .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+      .build()
+      .parseClaimsJws(token)
+      .getBody()
+      .getSubject();
+
+    return ResponseEntity.ok().body(new LoginCheckResponse(email));
   }
 }
