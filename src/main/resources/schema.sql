3da571d6febfd22bc54db205653f32a9655dc892
gomshiki <gomshiki@gmail.com>
2024-06-26 12:56:31 +0900

    test(schema): 예약시간, 예약테마 테스트 데이터 입력
    - ui 테스트 수행을 위해 초기 데이터 입력

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -29,3 +29,15 @@ CREATE TABLE reservation
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)            -- 외래키 추가
 );
+
+INSERT INTO reservation_time (start_at)
+VALUES ('10:00');
+INSERT INTO reservation_time (start_at)
+VALUES ('12:00');
+INSERT INTO reservation_time (start_at)
+VALUES ('14:00');
+
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마1', '설명1', '썸네일1');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마2', '설명2', '썸네일2');
