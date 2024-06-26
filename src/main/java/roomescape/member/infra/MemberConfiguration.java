5f162b2e70af1d7b57ceb2475a192028ad7dd3e7
gomshiki <gomshiki@gmail.com>
2024-06-26 13:10:30 +0900

    feat(ArgumentResolver): 요구사항 추가에 따른 ArgumentResolver 추가
    - 요구사항: 예약등록 시 토큰정보를 기반으로 회원정보 입력
      - ArgumentResolver 구현 및 어노테이션 추가
      - 추가한 ArgumentResolver를 WebMvcConfigurer 으로 등록

---
+++ src/main/java/roomescape/member/infra/MemberConfiguration.java
@@ -0,0 +1,22 @@
+package roomescape.member.infra;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class MemberConfiguration implements WebMvcConfigurer {
+
+    private final MemberArgumentResolver memberArgumentResolver;
+
+    public MemberConfiguration(MemberArgumentResolver memberArgumentResolver) {
+        this.memberArgumentResolver = memberArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberArgumentResolver);
+    }
+}
