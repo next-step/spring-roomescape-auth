e861d5e364c088b8c63da9295d7485981624d8fc
gomshiki <gomshiki@gmail.com>
2024-06-27 19:27:58 +0900

    refactor(Member): 데이터베이스 적용에 따른 엔티티 변경
    - ATOMIC_LONG 제거

--- src/main/java/roomescape/member/domain/Member.java
+++ src/main/java/roomescape/member/domain/Member.java
@@ -1,23 +1,15 @@
 package roomescape.member.domain;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 public class Member {
-
-    private static final AtomicLong ATOMIC_LONG = new AtomicLong(1);
 
     private Long id;
     private String name;
     private String email;
     private String password;
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
     public String getEmail() {
         return email;
@@ -32,8 +24,8 @@ public class Member {
         this.password = password;
     }
 
-    public Member(String name, String email, String password) {
-        this.id = ATOMIC_LONG.incrementAndGet();
+    public Member(Long id, String name, String email, String password) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
