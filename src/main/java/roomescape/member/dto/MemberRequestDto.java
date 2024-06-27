0ceb2b2dad02370e01caa4ef3005782928aa77f7
gomshiki <gomshiki@gmail.com>
2024-06-27 19:29:52 +0900

    chore: MemberRequestDto 에 사용하지 않는 필드, 생성자 제거
    - 필드 제거: id, email, password
    - 생성자, toString() 제거
    - getter 제거 : email, password

--- src/main/java/roomescape/member/dto/MemberRequestDto.java
+++ src/main/java/roomescape/member/dto/MemberRequestDto.java
@@ -2,45 +2,13 @@ package roomescape.member.dto;
 
 public class MemberRequestDto {
 
-    private Long id;
     private String name;
-    private String email;
-    private String password;
 
     public MemberRequestDto(String name) {
         this.name = name;
     }
 
-    public MemberRequestDto(Long id, String name, String email, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String toString() {
-        return "MemberRequestDto{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
     }
 }
