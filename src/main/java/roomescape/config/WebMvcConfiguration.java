858508614cb30024a1db9345b8891ce1598ad43e
yeeuniii <anna862700@gmail.com>
2024-06-28 14:02:55 +0900

    feat(step2): WebMvcConfiguration 생성

---
+++ src/main/java/roomescape/config/WebMvcConfiguration.java
@@ -0,0 +1,26 @@
+package roomescape.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.auth.application.CookieUtils;
+import roomescape.auth.application.JwtTokenProvider;
+import roomescape.auth.ui.LoginMemberArgumentResolver;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CookieUtils cookieUtils;
+
+    public WebMvcConfiguration(JwtTokenProvider jwtTokenProvider, CookieUtils cookieUtils) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.cookieUtils = cookieUtils;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver(jwtTokenProvider, cookieUtils));
+    }
+}
