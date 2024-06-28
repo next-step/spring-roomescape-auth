f405ca201a6e847f7e87b11fb991798621d7a912
yeeuniii <anna862700@gmail.com>
2024-06-29 01:50:04 +0900

    feat(step2): 사용자 하나 찾는 서비스 구현

--- src/main/java/roomescape/member/application/MemberService.java
+++ src/main/java/roomescape/member/application/MemberService.java
@@ -1,6 +1,7 @@
 package roomescape.member.application;
 
 import org.springframework.stereotype.Service;
+import roomescape.exception.NotFoundException;
 import roomescape.member.domain.MemberRepository;
 import roomescape.member.domain.entity.Member;
 import roomescape.member.ui.dto.MemberResponse;
@@ -18,5 +19,11 @@ public class MemberService {
     public List<MemberResponse> findAll() {
         List<Member> members = memberRepository.findAll();
         return MemberResponse.fromMembers(members);
+    }
+
+    public MemberResponse findOne(Long id) {
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> NotFoundException.of("id에 일치하는 사용자를 찾을 수 없습니다."));
+        return MemberResponse.from(member);
     }
 }
