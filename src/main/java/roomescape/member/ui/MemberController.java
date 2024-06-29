c2b0a26970699ebd5af6f420c12b0e6a738761a8
yeeuniii <anna862700@gmail.com>
2024-06-29 13:39:35 +0900

    feat(step2): 피드백 반영 - 사용자 단일 조회 api 구현 및 생성 api에 location 헤더 추가

--- src/main/java/roomescape/member/ui/MemberController.java
+++ src/main/java/roomescape/member/ui/MemberController.java
@@ -4,15 +4,13 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import roomescape.member.application.MemberService;
 import roomescape.member.application.SignUpService;
 import roomescape.member.ui.dto.MemberRequest;
 import roomescape.member.ui.dto.MemberResponse;
 
+import java.net.URI;
 import java.util.List;
 
 @Controller
@@ -27,14 +25,23 @@ public class MemberController {
     }
 
     @GetMapping
-    public ResponseEntity<List<MemberResponse>> read() {
+    public ResponseEntity<List<MemberResponse>> readAll() {
         List<MemberResponse> memberResponses = memberService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(memberResponses);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<MemberResponse> read(@PathVariable Long id) {
+        MemberResponse memberResponse = memberService.findOne(id);
+        return ResponseEntity.status(HttpStatus.OK).body(memberResponse);
     }
 
     @PostMapping
     public ResponseEntity<MemberResponse> create(@RequestBody @Valid MemberRequest memberRequest) {
         MemberResponse memberResponse = signUpService.signUp(memberRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(memberResponse);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .location(URI.create("/members/" + memberResponse.id()))
+                .body(memberResponse);
     }
 }
