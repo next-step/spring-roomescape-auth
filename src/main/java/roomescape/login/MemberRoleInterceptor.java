12820e26b077b4a2518a83f95544b9ab90a5eaf5
hhhhhwi <aab555586@gmail.com>
2024-06-30 21:08:43 +0900

    fix: /themes/**, /times/** URI 패턴 중 권한 체크 불필요한 api에 예외처리 - 테마 조회 : GET /themes - 시간 조회 : GET /times - 예약 가능 시간 조회 : GET /times/available

--- src/main/java/roomescape/login/MemberRoleInterceptor.java
+++ src/main/java/roomescape/login/MemberRoleInterceptor.java
@@ -4,13 +4,15 @@ import static roomescape.util.CookieUtils.getCookie;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.Arrays;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 import roomescape.error.exception.AuthenticationException;
 import roomescape.member.MemberRole;
 
 public class MemberRoleInterceptor implements HandlerInterceptor {
+
+    private final List<String> excludeGetUrls = List.of("/themes", "/times", "/times/available");
 
     private JwtTokenProvider jwtTokenProvider;
 
@@ -21,17 +23,31 @@ public class MemberRoleInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
         Object handler) throws Exception {
+        if(isExcludeToCheckRole(request)) {
+            return true;
+        }
         String token = getCookie(request, "token")
             .orElseThrow(AuthenticationException::new)
             .getValue();
 
-        MemberRole role = jwtTokenProvider.getMemberRole(token);
-
-        if (!role.equals(MemberRole.ADMIN)) {
-            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-            return false;
+        if(isAdmin(token)) {
+            return true;
         }
 
-        return true;
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        return false;
+    }
+
+    private boolean isExcludeToCheckRole(HttpServletRequest request) {
+        String method = request.getMethod();
+        String requestURI = request.getRequestURI();
+
+        return method.equals("GET") && excludeGetUrls.contains(requestURI);
+    }
+
+    private boolean isAdmin(String token) {
+        MemberRole role = jwtTokenProvider.getMemberRole(token);
+
+        return role.equals(MemberRole.ADMIN);
     }
 }
