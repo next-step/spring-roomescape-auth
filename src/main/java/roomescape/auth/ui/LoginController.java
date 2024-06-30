21c24990b90ccacd361f1da9dd4266e4f4d33629
yeeuniii <anna862700@gmail.com>
2024-07-01 02:01:53 +0900

    refactor(step2): 뷰 응답 컨트롤러를 WebMvcConfiguration 뷰 컨트롤러로 변경

--- src/main/java/roomescape/auth/ui/LoginController.java
+++ src/main/java/roomescape/auth/ui/LoginController.java
@@ -4,24 +4,29 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import roomescape.auth.application.AuthService;
 import roomescape.auth.ui.annotation.Login;
 import roomescape.auth.ui.dto.LoginCheckResponse;
 import roomescape.auth.ui.dto.LoginMember;
 import roomescape.auth.ui.dto.LoginRequest;
 
-@RestController
+@Controller
 @RequestMapping("login")
 public class LoginController {
     private final AuthService authService;
 
     public LoginController(AuthService loginService) {
         this.authService = loginService;
+    }
+
+    @GetMapping
+    public String readLoginPage() {
+        return "login";
     }
 
     @PostMapping
--- src/main/java/roomescape/config/WebMvcConfiguration.java
+++ src/main/java/roomescape/config/WebMvcConfiguration.java
@@ -2,6 +2,7 @@ package roomescape.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.auth.application.CookieUtils;
 import roomescape.auth.application.JwtTokenProvider;
@@ -18,6 +19,16 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     public WebMvcConfiguration(JwtTokenProvider jwtTokenProvider, CookieUtils cookieUtils) {
         this.jwtTokenProvider = jwtTokenProvider;
         this.cookieUtils = cookieUtils;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/admin/reservation").setViewName("admin/reservation");
+        registry.addViewController("/admin/time").setViewName("admin/time");
+        registry.addViewController("/admin/theme").setViewName("admin/theme");
+        registry.addViewController("/reservation").setViewName("reservation");
+        registry.addViewController("/signup").setViewName("signup");
     }
 
     @Override
