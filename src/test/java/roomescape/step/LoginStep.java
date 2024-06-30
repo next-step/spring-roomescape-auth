46beae855366e91932821218b4e363ac745d97b1
hhhhhwi <aab555586@gmail.com>
2024-06-30 20:33:32 +0900

    refactor: 중복 메서드 분리

--- src/test/java/roomescape/step/LoginStep.java
+++ src/test/java/roomescape/step/LoginStep.java
@@ -5,19 +5,24 @@ import io.restassured.http.ContentType;
 import roomescape.login.dto.LoginRequest;
 
 public class LoginStep {
+
+    public static String 회원_이메일 = "test@test.com";
+    public static String 회원_비밀번호 = "password";
+    public static String 관리자_이메일 = "admin@test.com";
+    public static String 관리자_비밀번호 = "password";
+
     public static String 회원_토큰_생성() {
-        return RestAssured.given().log().all()
-            .contentType(ContentType.JSON)
-            .body(new LoginRequest("test@test.com", "password"))
-            .when().post("/login")
-            .then().log().all().extract()
-            .cookie("token");
+        return 토큰_생성(회원_이메일, 회원_비밀번호);
     }
 
     public static String 관리자_토큰_생성() {
+        return 토큰_생성(관리자_이메일, 관리자_비밀번호);
+    }
+
+    public static String 토큰_생성(String email, String password) {
         return RestAssured.given().log().all()
             .contentType(ContentType.JSON)
-            .body(new LoginRequest("admin@test.com", "password"))
+            .body(new LoginRequest(email, password))
             .when().post("/login")
             .then().log().all().extract()
             .cookie("token");
